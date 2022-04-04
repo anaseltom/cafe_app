7cd58f69b2c905dbcaf7267f225ca68672904706
anas_vg <anoos2093@gmail.com>
2022-04-04 14:02:40 +0200

    Initial Commit

---
+++ cafe_api/app.js
@@ -0,0 +1,35 @@
+//express
+const express = require("express");
+// config imports
+const { serverPort } = require("./config.json");
+//router imports
+const categoryRouter = require("./router/category");
+const productRouter = require("./router/product");
+const userRouter = require("./router/user");
+const orderRouter = require("./router/order");
+const dashboardRouter = require("./router/dashboard");
+//sequelize imports
+const { sequelize } = require("./SQL/models");
+
+const app = express();
+app.use(express.json());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+//router imports
+app.use("/category", categoryRouter);
+app.use("/product", productRouter);
+app.use("/user", userRouter);
+app.use("/order", orderRouter);
+app.use("/dashboard", dashboardRouter);
+
+app.listen(serverPort, async () => {
+  console.log(`Up on ${serverPort}`);
+  await sequelize.sync().then(() => console.log("connected to database"));
+});
